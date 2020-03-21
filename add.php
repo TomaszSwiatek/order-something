9a01f8ac6a5ca3c0157d9ab455cb17eea839cc4d
Tomasz Swiatek <tomasswiatek@gmail.com>
2020-03-21 19:35:40 +0100

    added backend validation to add order form

--- add.php
+++ add.php
@@ -2,15 +2,56 @@
 //isset method checks is send everything to a server
 // $_GET - it is globsal array where we store parameters/data sent with get request. so it checks is sent submit  value to this array or not? (this value is send after click submit button/input)
 // if (isset($_GET['submit'])) {
-//     echo $_GET['email'];
+//     echo $_GET['email']; //get methods shows what we send in our browser input
 //     echo $_GET['title'];
 //     echo $_GET['ingredients'];
 // }
 if (isset($_POST['submit'])) {
-    echo $_POST['email'];
-    echo $_POST['title'];
-    echo $_POST['ingredients'];
-}
+
+
+    //check email
+    if (empty($_POST['email'])) { //if field is empty:
+        echo 'An email is required <br>';
+    } else {
+        // if not:
+        // echo htmlspecialchars($_POST['email']);
+        //htmlspecialchars() method cause same thing like we would do in js with text method instead html, to avoid xss atacks.
+
+        $email = $_POST['email'];
+        //WE PLACE negation operator becouse we want to show message, if email variable isn't proper.
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo 'this field must be a valid email address';
+            //meth. filter_var takes to arguments: first the variable we want to filter, second - type of filter that we want to apply
+
+        }
+    }
+
+    //check title
+    if (empty($_POST['title'])) { //if field is empty:
+        echo 'An title is required <br>';
+    } else {
+        // if not do the code:
+        $title = $_POST['title'];
+        // we have to negate becouse we want to show info, we input content doesnt match to pattern.
+        if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {  //this is how we match something to regular expression. first arg. is an regular expression, second: varieble we match to our own expression
+            echo 'Title must be letters and spaces only';
+        }
+    }
+
+    //check ingredients
+    if (empty($_POST['ingredients'])) { //if field is empty:
+        echo 'At least one ingredient is required <br>';
+    } else {
+        // if not:
+        $ingredients = $_POST['ingredients'];
+        if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) { //reg ex - list comma separated only.
+            //if field isnt filled out properly (so error handling):
+            echo 'Please write out only ingredients and separate them with comma!';
+        }
+    }
+};
+//END
+
 
 ?>
 
