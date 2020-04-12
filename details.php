4746e71a37b4397af12fdc231226b0985efd6705
Tomasz Swiatek <tomasswiatek@gmail.com>
2020-04-12 17:19:17 +0200

    added delete item feature to each detail page

--- details.php
+++ details.php
@@ -1,5 +1,19 @@
 <?php
 include('config/db_connect.php');
+// form to delete this recipe:
+if (isset($_POST['delete'])) {
+    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+    $sql = "DELETE FROM orders WHERE id = $id_to_delete";
+
+    if (mysqli_query($conn, $sql)) {
+        //delete success
+        header('Location: index.php');
+    } else {
+        //delete failure
+        echo 'error occurred by: ' . mysqli_error($conn);
+    }
+}
+
 // check get request is proper
 if (isset($_GET['id'])) {
 
@@ -37,7 +51,12 @@ if (isset($_GET['id'])) {
                 </ul>
             <?php }; ?>
         </p>
+        <form action="details.php" method="post">
 
+            <input type="hidden" name="id_to_delete" value="<?php echo $recipe['id']; ?>">
+            <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+
+        </form>
     <?php else : ?>
         <h4>No such a recipe!</h4>
     <?php endif; ?>
